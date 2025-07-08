e0cb91cca56670f4234206848b4842c360083883
seolyeong333 <33qqpp@naver.com>
2025-07-08 16:24:11 +0900

    add Feign Client

---
+++ admin-service/src/main/java/com/jigubangbang/admin_service/model/chat_service/InquiryNotificationRequestDto.java
@@ -0,0 +1,20 @@
+package com.jigubangbang.admin_service.model.chat_service;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InquiryNotificationRequestDto {
+
+    private String userId;
+    private String title; 
+    private String message;
+    private String relatedUrl;
+    private String senderId;
+
+}
