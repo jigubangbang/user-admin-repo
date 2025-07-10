67375237fbde5528fdfc4685225d4072138fe50b
nlhyll <pnhmms@gmail.com>
2025-07-10 17:11:20 +0900

    댓글관리-콘텐츠아이디 추가

--- admin-service/src/main/java/com/jigubangbang/admin_service/model/AdminCommentDto.java
+++ admin-service/src/main/java/com/jigubangbang/admin_service/model/AdminCommentDto.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 @Setter
 public class AdminCommentDto {
     private int commentId;              // 댓글 ID (board_comment.id / feed_comment.id / travelmate_comment.id)
+    private int originalPostId;         // 해당 댓글이 있는 콘텐츠 ID
     private String contentType;         // "community" or "feed" or "group"
     private String content;             // 댓글 내용
     private String userId;              // 작성자 ID
