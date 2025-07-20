529bc8d192d43d7f2c7f3f18dd66c2976eb88c84
junhwan2000 <j8428820@naver.com>
2025-07-20 13:00:01 +0900

    de fe

--- payment-service/src/main/java/com/jigubangbang/payment_service/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/jigubangbang/payment_service/PaymentServiceApplication.java
@@ -2,11 +2,9 @@ package com.jigubangbang.payment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableFeignClients
 @EnableScheduling // 스케줄러 활성화 (기존)
 public class PaymentServiceApplication {
 
